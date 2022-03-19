4e2df63196e106abf863773667c2cb52b7cb053a
maheladissa <62756370+maheladissa@users.noreply.github.com>
2022-03-19 17:57:00 +0530

    Re adding user model & retrofit config

---
+++ app/src/main/java/com/seng22243/apihandling/api/RetrofitConfigurations.kt
@@ -0,0 +1,28 @@
+package com.seng22243.apihandling.api
+
+import com.seng22243.apihandling.model.User
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface UserAPIService {
+    @GET("users")
+    fun getUsers(): Call<List<User>>
+
+    @GET("users/{userId}")
+    fun getUser(@Path("userId") id: String): Call<User>
+
+    companion object {
+        val API_URL = "https://jsonplaceholder.typicode.com"
+        fun create(): UserAPIService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(UserAPIService::class.java)
+        }
+    }
+}
